b708fe676de24df085d9e821ddab5cc1c5b8aa93
U-PC-BUREAU\jacques <jdesmis@gmail.com>
2018-07-09 13:08:07 +0200

    Fixed typing error in one spectral data

--- rtengine/colortemp.h
+++ rtengine/colortemp.h
@@ -228,7 +228,7 @@ public:
     static const double ColorWhitepetunia[97];
     static const double ColorWhitepelargonia[97];
     static const double ColorWhitepaeivaen[97];
-    static const double JDC468_B14_75greyspect[97];
+    static const double JDC468_B14_75yelspect[97];
     static const double Colorblue_spect[97];
     static const double ColorGreenkoriste[97];
     static const double ColorGreenpoimulehti[97];
@@ -241,6 +241,7 @@ public:
     static const double ColorBluemalvikki[97];
     static const double ColorBlackmaito[97];
     static const double ColorOrangpihlaja[97];
+    static const double ColorBlackpihlaja[97];
 
     static const double JDC468_OraO18_spect[97]; //468 O18
     static const double JDC468_OraD17_spect[97]; //468 D17
